19d1d1cfc24840d2720a1cf754fbc9eae12aa031
Marc Riera <mrc2407@gmail.com>
2012-07-15 18:46:06 +0200

    Add forgotten header to common.c file

--- common.c
+++ common.c
@@ -1,1 +1,9 @@
+/********************************************************
+ * Xarxes de Computadors 2: Trivial FTP                 *
+ * Autor:  Marc Riera      - 1242263                    *
+ *                                                      *
+ * Fitxer: common.c                                     *
+ * Descripció: Codi comú pel client servidor TFTP.      *
+ ********************************************************/
+
 #include "common.h"
